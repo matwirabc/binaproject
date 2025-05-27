77d03543ec5b99e723b32c04242ad6d060380e2d
Jairzinho2022 <122702141+Jairzinho2022@users.noreply.github.com>
2025-05-27 08:43:05 +0200

    Projekt aufgeräumt – unnötige Ordner entfernt

--- Einwohnerzahlen/main.py
+++
@@ -1,8 +0,0 @@
-from scripts.transform import bereinige_rohdaten
-
-eingabe = "data/Rohdaten/Rohdatei_Einwohner.xlsx"
-ausgabe = "data/Verarbeitet/Struktur_Bereinigt.xlsx"
-
-# Bereinigen und speichern
-daten_bereinigt = bereinige_rohdaten(eingabe, ausgabe)
-
